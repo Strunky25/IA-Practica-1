5cbf92be66428d127b5777cce0fa45ad611029a6
Strunky25 <jonathansalisburyvega1@gmail.com>
2021-10-19 11:02:55 +0200

    Create branch

--- src/agents/Bitxo1.java
+++ src/agents/Bitxo1.java
@@ -1,8 +1,9 @@
 package agents;
 
-import java.lang.Math;
-
 // Exemple de Bitxo
+
+import java.util.Random;
+
 public class Bitxo1 extends Agent {
 
     static final int PARET = 0;
@@ -10,157 +11,118 @@ public class Bitxo1 extends Agent {
     static final int RES = -1;
 
     static final int ESQUERRA = 0;
-    static final int ENDAVANT = 1;
     static final int CENTRAL = 1;
     static final int DRETA = 2;
-    static final int ENRERE = 3;
-    static final int DONAVOLTA = 4;
 
-    static final int ALERTA = 0;
-    static final int A = 1;
-    static final int B = 2;
-
-    int repetir;
-    int accio;
-    int estatBitxo;
-    int objI;
-    boolean EsticMirant;
-    //boolean heMirat;
-    Estat estat;
-    Objecte[] obj;
-
-    //Esto nos permite encontrar el recurso nuestro mas cercano
-    Objecte recursPropiLocal;
-    Objecte recursPropiGlobal;
-    Objecte recursEnemicLocal;
-    Objecte recursEnemicGlobal;
-    int dist_recurs_Propia_Global;
-    int dist_recurs_Propia_Local;
-    int dist_recurs_Enemic_Global;
-    int dist_recurs_Enemic_Local;
-    boolean heTrobatPropi;
-    boolean heTrobatEnemic;
-
-    int sectorLocal;
-    int sectorGlobal;
-
-    int recursosActuals;
-
-    int graus;
-    int girs;
-
-    int polla = 0;
+    private Estat estat;
+    private Random random;           
+    private Accio accio;
+    private int repetir, darrer_gir;
+    private boolean mirant;
 
     public Bitxo1(Agents pare) {
-        super(pare, "Bitxo1", "imatges/robotank1.gif");
+        super(pare, "Lift", "imatges/robotank1.gif");
     }
 
     @Override
     public void inicia() {
-
         // atributsAgents(v,w,dv,av,ll,es,hy)
-        graus = 45;
-        int cost = atributsAgent(7, 9, 600, graus, 40, 5, 0);
-        System.out.println("Cost total:" + cost);
-
-        // Inicialització de variables que utilitzaré al meu comportament   
+        int cost = atributsAgent(6, 8, 600, 30, 23, 5, 5);
+        System.out.println("Cost total: " + cost);
+        // Inicialització de variables que utilitzaré al meu comportament
         repetir = 0;
-        objI = 0;
-        estatBitxo = ALERTA;
-        EsticMirant = false;
-        heTrobatPropi = false;
-        // heMirat = false;
-        sectorLocal = 0;
-        sectorGlobal = 0;
-        recursosActuals = 0;
-
-        dist_recurs_Propia_Global = 9999;
-
-        girs = 1;//(int) Math.ceil(360 / graus);
+        accio = Accio.ENDAVANT;
+        mirant = false;
+        random = new Random();
     }
 
     @Override
     public void avaluaComportament() {
-        estat = estatCombat();
-        if (repetir != 0) {
-            repetirAccio();
-            repetir--;
+        if(!repetirAccio()){ 
+            estat = estatCombat();
+            deteccioAliment();
+            deteccioParet();         
+        }
+    }
+    
+    private void deteccioParet(){
+        if(estat.enCollisio){
+            accio = Accio.VOLTEJ;
+            repetir = 1;
+        }else if(estat.distanciaVisors[CENTRAL] < 30 &&
+                estat.objecteVisor[CENTRAL] == PARET){
+            if(random.nextBoolean()) accio = Accio.DRETA;
+            else accio = Accio.ESQUERRA;
+            repetir = 3;    
+        } else if (estat.distanciaVisors[ESQUERRA] < 40 &&
+                estat.objecteVisor[ESQUERRA] == PARET){
+            accio = Accio.DRETA;
+            repetir = 3;
+        } else if (estat.distanciaVisors[DRETA] < 40 &&
+                estat.objecteVisor[DRETA] == PARET){
+            accio = Accio.ESQUERRA;
+            repetir = 3;
         } else {
-            if (estat.enCollisio) {
-                atura();
-                accio = ENRERE;
-                repetir = 5;
-            } else {
-
-                cerca();
-                if (heTrobatEnemic && estat.llançaments > 0) {
-                    mira(recursEnemicGlobal);
-                    llança();
-                } else if (heTrobatEnemic) {
-                    mira(recursPropiGlobal);
-                }
-
-                if (estat.distanciaVisors[ESQUERRA] < 20 && (estat.objecteVisor[ESQUERRA] == PARET)) {
-                    accio = DRETA;
-                    repetir = 1;
-                } else if (estat.distanciaVisors[DRETA] < 20 && (estat.objecteVisor[DRETA] == PARET)) {
-                    accio = ESQUERRA;
-                    repetir = 1;
-                }
-                endavant();
-            }
+            endavant();
         }
     }
-
-    private void repetirAccio() {
-        switch (accio) {
-            case 0:
-                //esquerra();
-                gira(30);
-                break;
-            case 1:
-                endavant();
-                break;
-            case 2:
-                //dreta();
-                gira(-30);
-                break;
-            case 3:
-                enrere();
-                if (repetir == 1) {
-                    gira(30);
-                }
-                break;
-            case 4:
-                gira(graus * 2);
-                cerca();
-                sectorLocal++;
-                break;
-        }
-    }
-
-    private void cerca() {
-        obj = estat.objectes;
-        heTrobatPropi = false;
-        heTrobatEnemic = false;
-        recursPropiLocal = null;
-        recursEnemicLocal = null;
-        dist_recurs_Propia_Global = 9999;
-        dist_recurs_Enemic_Global = 9999;
-        for (Objecte o : obj) {
-            if (o != null && o.agafaTipus() == (100 + estat.id)) {
-                if (o.agafaDistancia() < dist_recurs_Propia_Global) {
-                    dist_recurs_Propia_Global = o.agafaDistancia();
-                    recursPropiGlobal = o;
-                    heTrobatPropi = true;
-                }
-            } else if (o != null && (o.agafaTipus() >= 100) && (o.agafaTipus() != (100 + estat.id))) {
-                if (o.agafaDistancia() < dist_recurs_Enemic_Global) {
-                    dist_recurs_Enemic_Global = o.agafaDistancia();
-                    recursEnemicGlobal = o;
-                    heTrobatPropi = true;
+    
+    private void deteccioAliment(){
+        if(estat.numObjectes > 0 && estat.veigAlgunRecurs){
+            int distanciaMin = Integer.MAX_VALUE;
+            Objecte obj = null;
+            for(Objecte objActual: estat.objectes){
+                if(objActual != null){
+                    int distanciaObjActual = objActual.agafaDistancia();
+                    if(objActual.agafaTipus() == (100 + estat.id) &&
+                            distanciaObjActual < distanciaMin){
+                        distanciaMin = distanciaObjActual;
+                        obj = objActual;
+                    }
                 }
             }
+            if(obj != null){
+                mira(obj);
+                mirant = true;
+            } else mirant = false;
+        } else mirant = false;
+    }
+    
+    private boolean repetirAccio(){
+        if(repetir != 0){
+            switch(accio){
+                case ESQUERRA:
+                    darrer_gir = 10 + random.nextInt(10);
+                    break;
+                case DRETA:
+                    darrer_gir = -(10 + random.nextInt(10));
+                    break;
+                case VOLTEJ:
+                    darrer_gir = 180 + (random.nextInt(90) - 45);
+                    break;  
+                case DESFER:
+                    darrer_gir = -(darrer_gir);
+                    break;
+            }
+            gira(darrer_gir);
+            comprobaMirant();
+            repetir--;
+            return true;
+        } else return false;
+    }
+    
+    private void comprobaMirant(){
+        if(mirant){
+            accio = Accio.DESFER;
+            repetir = 1;
         }
+    }
+    
+    private enum Accio{
+        ESQUERRA,
+        DRETA,
+        VOLTEJ,
+        ENDAVANT, 
+        DESFER
     }
 }
