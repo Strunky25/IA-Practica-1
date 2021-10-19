58e2fe17f991f6b95c47d79977cc669f2d631fbe
Strunky25 <jonathansalisburyvega1@gmail.com>
2021-10-19 22:48:49 +0200

    Update Bitxo3.java

--- src/agents/Bitxo3.java
+++ src/agents/Bitxo3.java
@@ -1,11 +1,9 @@
 package agents;
 
-// Exemple de Bitxo
-
 import java.util.Random;
 
 /*
-    BITXO JONNY
+    BITXO NOSOTROS <3
 */
 
 public class Bitxo3 extends Agent {
@@ -51,6 +49,94 @@ public class Bitxo3 extends Agent {
         }
     }
     
+    private enum Accio{
+        ESQUERRA,
+        DRETA,
+        VOLTEJ,
+        ENDAVANT, 
+        DESFER,
+        CONTINUA
+    }
+    
+    private boolean repetirAccio(){
+        if(repetir != 0){
+            switch(accio){
+                case ESQUERRA:
+                    darrer_gir = 10 + random.nextInt(10);
+                    break;
+                case DRETA:
+                    darrer_gir = -(10 + random.nextInt(10));
+                    break;
+                case VOLTEJ:
+                    darrer_gir = 180 + (random.nextInt(90) - 45);
+                    break;  
+                case DESFER:
+                    darrer_gir = -(darrer_gir);
+                    break;
+                case CONTINUA:
+                    break;
+            }
+            gira(darrer_gir);
+            //comprobaMirant();
+            repetir--;
+            return true;
+        } else return false;
+    }
+    
+    private void comprobaMirant(){
+        if(mirant){
+            accio = Accio.DESFER;
+            repetir = 1;
+        }
+    }
+    
+    private void deteccioRecursos(){
+        if(estat.numObjectes > 0 && estat.veigAlgunRecurs){
+            
+        }
+//            int distanciaMin = MAX_VISIO;
+//            int distanciaActual;
+//            int distanciaMinRecAliado = MAX_VISIO;
+//            int distanciaMinRecEnemigo = MAX_VISIO;
+//            Objecte objRecAliadoMasCercano = null;
+//            Objecte objRecEnemigoMasCercano = null;
+//            for(Objecte objActual: estat.objectes){
+//                if(objActual != null){
+//                    distanciaActual = objActual.agafaDistancia();
+//                    if(distanciaActual < distanciaMin){
+//                        distanciaMin = distanciaActual;
+//                        if(esRecursAliat(objActual)){
+//                            distanciaMinRecAliado = distanciaActual;
+//                            objRecAliadoMasCercano = objActual;
+//                        } else if(objActual.agafaTipus() >= 100 
+//                                && !esRecursAliat(objActual)){
+//                            distanciaMinRecEnemigo = distanciaActual;
+//                            objRecEnemigoMasCercano = objActual;
+//                        }
+//                    }
+//                }
+//            }
+//            if(distanciaMinRecEnemigo < MAX_VISIO &&
+//                    estat.llançaments > 0 && !estat.llançant
+//                    && objRecEnemigoMasCercano != null){
+//                System.out.println(objRecEnemigoMasCercano.agafaTipus());
+//                mira(objRecEnemigoMasCercano);
+//                llança();
+//                mirant = true;
+//            }
+//            /*
+//            if(distanciaMinRecAliado < MAX_VISIO 
+//                    && objRecAliadoMasCercano != null){
+//                mira(objRecAliadoMasCercano);
+//                mirant = true;
+//            } else mirant = false;*/
+//        } else mirant = false;
+    }
+    
+    private boolean esRecursAliat(Objecte obj){
+        return obj.agafaTipus() == (100 + estat.id);
+    }
+    
     private void deteccioParet(){
         if(estat.enCollisio){
             accio = Accio.VOLTEJ;
@@ -71,88 +157,5 @@ public class Bitxo3 extends Agent {
         } else {
             endavant();
         }
-    }
-    
-    private void deteccioRecursos(){
-        if(estat.numObjectes > 0 && estat.veigAlgunRecurs){
-            int distanciaMin = MAX_VISIO;
-            int distanciaActual;
-            int distanciaMinRecAliado = MAX_VISIO;
-            int distanciaMinRecEnemigo = MAX_VISIO;
-            Objecte objRecAliadoMasCercano = null;
-            Objecte objRecEnemigoMasCercano = null;
-            for(Objecte objActual: estat.objectes){
-                if(objActual != null){
-                    distanciaActual = objActual.agafaDistancia();
-                    if(distanciaActual < distanciaMin){
-                        distanciaMin = distanciaActual;
-                        if(esRecursAliat(objActual)){
-                            distanciaMinRecAliado = distanciaActual;
-                            objRecAliadoMasCercano = objActual;
-                        } else if(objActual.agafaTipus() >= 100 
-                                && !esRecursAliat(objActual)){
-                            distanciaMinRecEnemigo = distanciaActual;
-                            objRecEnemigoMasCercano = objActual;
-                        }
-                    }
-                }
-            }
-            if(distanciaMinRecEnemigo < MAX_VISIO &&
-                    estat.llançaments > 0 && !estat.llançant
-                    && objRecEnemigoMasCercano != null){
-                System.out.println(objRecEnemigoMasCercano.agafaTipus());
-                mira(objRecEnemigoMasCercano);
-                llança();
-                mirant = true;
-            }
-            /*
-            if(distanciaMinRecAliado < MAX_VISIO 
-                    && objRecAliadoMasCercano != null){
-                mira(objRecAliadoMasCercano);
-                mirant = true;
-            } else mirant = false;*/
-        } else mirant = false;
-    }
-    
-    private boolean esRecursAliat(Objecte obj){
-        return obj.agafaTipus() == (100 + estat.id);
-    }
-    
-    private boolean repetirAccio(){
-        if(repetir != 0){
-            switch(accio){
-                case ESQUERRA:
-                    darrer_gir = 10 + random.nextInt(10);
-                    break;
-                case DRETA:
-                    darrer_gir = -(10 + random.nextInt(10));
-                    break;
-                case VOLTEJ:
-                    darrer_gir = 180 + (random.nextInt(90) - 45);
-                    break;  
-                case DESFER:
-                    darrer_gir = -(darrer_gir);
-                    break;
-            }
-            gira(darrer_gir);
-            comprobaMirant();
-            repetir--;
-            return true;
-        } else return false;
-    }
-    
-    private void comprobaMirant(){
-        if(mirant){
-            accio = Accio.DESFER;
-            repetir = 1;
-        }
-    }
-    
-    private enum Accio{
-        ESQUERRA,
-        DRETA,
-        VOLTEJ,
-        ENDAVANT, 
-        DESFER
     }
 }
