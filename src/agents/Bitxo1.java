40b3fb547732a2917454388424dac64661654b10
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2021-10-20 11:19:51 +0200

    Update Bitxo 1

--- src/agents/Bitxo1.java
+++ src/agents/Bitxo1.java
@@ -1,10 +1,10 @@
 package agents;
 
-/*
-    BITXO JONNY
-*/
 import java.util.Random;
 
+/*
+    BITXO NOSOTROS <3
+ */
 public class Bitxo1 extends Agent {
 
     static final int PARET = 0;
@@ -15,14 +15,16 @@ public class Bitxo1 extends Agent {
     static final int CENTRAL = 1;
     static final int DRETA = 2;
 
+    private static final int MAX_VISIO = 405;
+
     private Estat estat;
-    private Random random;           
+    private Random random;
     private Accio accio;
     private int repetir, darrer_gir;
     private boolean mirant;
 
     public Bitxo1(Agents pare) {
-        super(pare, "Jonathan", "imatges/robotank1.gif");
+        super(pare, "Nosotros <3", "imatges/robotank1.gif");
     }
 
     @Override
@@ -33,65 +35,30 @@ public class Bitxo1 extends Agent {
         // Inicialització de variables que utilitzaré al meu comportament
         repetir = 0;
         accio = Accio.ENDAVANT;
-        mirant = false;
         random = new Random();
     }
 
     @Override
     public void avaluaComportament() {
-        if(!repetirAccio()){ 
+        if (!repetirAccio()) {
             estat = estatCombat();
-            deteccioAliment();
-            deteccioParet();         
+            deteccioRecursos();
+            deteccioParet();
         }
     }
-    
-    private void deteccioParet(){
-        if(estat.enCollisio){
-            accio = Accio.VOLTEJ;
-            repetir = 1;
-        }else if(estat.distanciaVisors[CENTRAL] < 30 &&
-                estat.objecteVisor[CENTRAL] == PARET){
-            if(random.nextBoolean()) accio = Accio.DRETA;
-            else accio = Accio.ESQUERRA;
-            repetir = 3;    
-        } else if (estat.distanciaVisors[ESQUERRA] < 40 &&
-                estat.objecteVisor[ESQUERRA] == PARET){
-            accio = Accio.DRETA;
-            repetir = 3;
-        } else if (estat.distanciaVisors[DRETA] < 40 &&
-                estat.objecteVisor[DRETA] == PARET){
-            accio = Accio.ESQUERRA;
-            repetir = 3;
-        } else {
-            endavant();
-        }
+
+    private enum Accio {
+        ESQUERRA,
+        DRETA,
+        VOLTEJ,
+        ENDAVANT,
+        DESFER,
+        CONTINUA
     }
-    
-    private void deteccioAliment(){
-        if(estat.numObjectes > 0 && estat.veigAlgunRecurs){
-            int distanciaMin = Integer.MAX_VALUE;
-            Objecte obj = null;
-            for(Objecte objActual: estat.objectes){
-                if(objActual != null){
-                    int distanciaObjActual = objActual.agafaDistancia();
-                    if(objActual.agafaTipus() == (100 + estat.id) &&
-                            distanciaObjActual < distanciaMin){
-                        distanciaMin = distanciaObjActual;
-                        obj = objActual;
-                    }
-                }
-            }
-            if(obj != null){
-                mira(obj);
-                mirant = true;
-            } else mirant = false;
-        } else mirant = false;
-    }
-    
-    private boolean repetirAccio(){
-        if(repetir != 0){
-            switch(accio){
+
+    private boolean repetirAccio() {
+        if (repetir != 0) {
+            switch (accio) {
                 case ESQUERRA:
                     darrer_gir = 10 + random.nextInt(10);
                     break;
@@ -100,30 +67,90 @@ public class Bitxo1 extends Agent {
                     break;
                 case VOLTEJ:
                     darrer_gir = 180 + (random.nextInt(90) - 45);
-                    break;  
+                    break;
                 case DESFER:
                     darrer_gir = -(darrer_gir);
                     break;
+                case CONTINUA:
+                    break;
             }
             gira(darrer_gir);
-            comprobaMirant();
+            //comprobaMirant();
             repetir--;
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
-    
-    private void comprobaMirant(){
-        if(mirant){
+
+    private void comprobaMirant() {
+        if (mirant) {
             accio = Accio.DESFER;
             repetir = 1;
         }
     }
-    
-    private enum Accio{
-        ESQUERRA,
-        DRETA,
-        VOLTEJ,
-        ENDAVANT, 
-        DESFER
+
+    private void deteccioRecursos() {
+        if (estat.numObjectes > 0 && estat.veigAlgunRecurs) {
+            int distActualRecAli, distMinRecAli = Integer.MAX_VALUE,
+                    distActualRecEne, distMinRecEne = MAX_VISIO;
+            Objecte recAliMesProper, recEneMesProper;
+            recAliMesProper = recEneMesProper = null;
+            for (Objecte objActual : estat.objectes) {
+                if (objActual != null) {
+                    int tipusRec = objActual.agafaTipus();
+                    if (esRecAliEsc(tipusRec)) {
+                        distActualRecAli = objActual.agafaDistancia();
+                        if (distActualRecAli < distMinRecAli) {
+                            distMinRecAli = distActualRecAli;
+                            recAliMesProper = objActual;
+                        }
+                    } else if (tipusRec > 100 && !esRecAliEsc(tipusRec)) {
+                        distActualRecEne = objActual.agafaDistancia();
+                        if (distActualRecEne < distMinRecEne) {
+                            distMinRecEne = distActualRecEne;
+                            recEneMesProper = objActual;
+                        }
+                    }
+                }
+            }
+            if (recEneMesProper != null && estat.llançaments > 0 
+                    && !estat.llançant && distMinRecEne < distMinRecAli) {
+                mira(recEneMesProper);
+                llança();
+            }
+            if (recAliMesProper != null) {
+                mira(recAliMesProper);
+            }
+        }
+    }
+
+    private boolean esRecAliEsc(int tipus) {
+        return tipus == (100 + estat.id) || tipus == Estat.ESCUT;
+    }
+
+    private void deteccioParet() {
+        if (estat.enCollisio) {
+            accio = Accio.VOLTEJ;
+            repetir = 1;
+        } else if (estat.distanciaVisors[CENTRAL] < 30
+                && estat.objecteVisor[CENTRAL] == PARET) {
+            if (random.nextBoolean()) {
+                accio = Accio.DRETA;
+            } else {
+                accio = Accio.ESQUERRA;
+            }
+            repetir = 3;
+        } else if (estat.distanciaVisors[ESQUERRA] < 40
+                && estat.objecteVisor[ESQUERRA] == PARET) {
+            accio = Accio.DRETA;
+            repetir = 3;
+        } else if (estat.distanciaVisors[DRETA] < 40
+                && estat.objecteVisor[DRETA] == PARET) {
+            accio = Accio.ESQUERRA;
+            repetir = 3;
+        } else {
+            endavant();
+        }
     }
 }
